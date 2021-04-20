86c67b787e5600b4abbc7579101ce6f7bc6c426b
Mikmondi <mikmondi@gmail.com>
2021-04-20 08:11:51 +0300

    added function to the services icons

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
       $(".design-show").toggle();
     });
   });
+
+  $(document).ready(function() {
+    $(".servicesdevelopment").click(function() {
+      $(".development-hide").toggle();
+      $(".development-show").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $(".servicesproduct").click(function() {
+      $(".product-hide").toggle();
+      $(".product-show").toggle();
+    });
+  });
